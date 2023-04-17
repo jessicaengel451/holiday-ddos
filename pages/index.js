badda226815be9d3e465f3f0dc4eae64105e5b9a
jessicaengel451 <72846465+jessicaengel451@users.noreply.github.com>
2023-04-17 01:13:16 -0600

    working page with the empty year view

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,51 @@
+// import Head from 'next/head'
+// import Header from '@components/Header'
+// import Footer from '@components/Footer'
+// import CalHeatmap from 'cal-heatmap';
+
+// // Optionally import the CSS
+// import 'cal-heatmap/cal-heatmap.css';
+
+// const cal = new CalHeatmap();
+// cal.paint({});
+
+// export default function Home() {
+//   return (
+//     <div className="container">
+//       <Head>
+//         <title>Next.js Starter!</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <main>
+//         <Header title="Welcome to my app!" />
+//         <div id="cal-heatmap"></div>
+//       </main>
+
+//       <Footer />
+//     </div>
+//   )
+// }
+
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import CalHeatmap from 'cal-heatmap';
+import { useEffect, useRef } from 'react';
+
+// Optionally import the CSS
+import 'cal-heatmap/cal-heatmap.css';
 
 export default function Home() {
+  const calHeatmapRef = useRef(null);
+
+  useEffect(() => {
+    if (calHeatmapRef.current) {
+      const cal = new CalHeatmap();
+      cal.paint(calHeatmapRef.current);
+    }
+  }, []);
+
   return (
     <div className="container">
       <Head>
@@ -12,12 +55,11 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <div id="cal-heatmap" ref={calHeatmapRef}></div>
       </main>
 
       <Footer />
     </div>
   )
 }
+
